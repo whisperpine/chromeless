4387113d96a04cb16ee0131212c88b5d63c0e370
Yusong <yusonglai64@gmail.com>
2025-10-11 21:24:05 +0800

    feat: add 16 slots to disable Chrome's default shortcuts

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,7 @@
-export default defineBackground(() => {
+export default defineBackground((): void => {
   console.log("Hello background!", { id: browser.runtime.id });
+  // Commands (keyboard shortcuts) must be handled to disable Chrome's defaults.
+  browser.commands.onCommand.addListener((command: string): void => {
+    console.log("Command triggered:", command);
+  });
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,25 @@
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  manifest: {
+    commands: {
+      doNothing1: { description: "Do nothing" },
+      doNothing2: { description: "Do nothing" },
+      doNothing3: { description: "Do nothing" },
+      doNothing4: { description: "Do nothing" },
+      doNothing5: { description: "Do nothing" },
+      doNothing6: { description: "Do nothing" },
+      doNothing7: { description: "Do nothing" },
+      doNothing8: { description: "Do nothing" },
+      doNothing9: { description: "Do nothing" },
+      doNothing10: { description: "Do nothing" },
+      doNothing11: { description: "Do nothing" },
+      doNothing12: { description: "Do nothing" },
+      doNothing13: { description: "Do nothing" },
+      doNothing14: { description: "Do nothing" },
+      doNothing15: { description: "Do nothing" },
+      doNothing16: { description: "Do nothing" },
+    },
+  },
+});
